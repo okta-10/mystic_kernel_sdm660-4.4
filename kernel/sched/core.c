bc6eebb9c494349a34aa1520f045a38128e611bf
Peter Zijlstra <peterz@infradead.org>
2017-02-21 14:40:35 +0100

    sched/core: Simplify update_rq_clock() in __schedule()
    Instead of relying on deactivate_task() to call update_rq_clock() and
    handling the case where it didn't happen (task_on_rq_queued),
    unconditionally do update_rq_clock() and skip any further updates.
    
    This also avoids a double update on deactivate_task() + ttwu_local().
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2214,7 +2214,7 @@ static void try_to_wake_up_local(struct task_struct *p)
 
 		walt_update_task_ravg(rq->curr, rq, TASK_UPDATE, wallclock, 0);
 		walt_update_task_ravg(p, rq, TASK_WAKE, wallclock, 0);
-		ttwu_activate(rq, p, ENQUEUE_WAKEUP);
+                ttwu_activate(rq, p, ENQUEUE_WAKEUP | ENQUEUE_NOCLOCK);
 	}
 
 	ttwu_do_wakeup(rq, p, 0);
@@ -3483,13 +3483,14 @@ static void __sched notrace __schedule(bool preempt)
 	lockdep_pin_lock(&rq->lock);
 
 	rq->clock_skip_update <<= 1; /* promote REQ to ACT */
+        update_rq_clock(rq);
 
 	switch_count = &prev->nivcsw;
 	if (!preempt && prev->state) {
 		if (unlikely(signal_pending_state(prev->state, prev))) {
 			prev->state = TASK_RUNNING;
 		} else {
-			deactivate_task(rq, prev, DEQUEUE_SLEEP);
+			deactivate_task(rq, prev, DEQUEUE_SLEEP | DEQUEUE_NOCLOCK);
 			prev->on_rq = 0;
 
 			if (prev->in_iowait) {
@@ -3512,9 +3513,6 @@ static void __sched notrace __schedule(bool preempt)
 		}
 		switch_count = &prev->nvcsw;
 	}
-
-	if (task_on_rq_queued(prev))
-		update_rq_clock(rq);
 
 	next = pick_next_task(rq, prev);
 	wallclock = walt_ktime_clock();
