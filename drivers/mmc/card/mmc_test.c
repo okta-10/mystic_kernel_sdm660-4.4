293f31ce6eb35398952e89ae64d2c4e6cee43695
Markus Elfring <elfring@users.sourceforge.net>
2017-05-19 13:15:27 +0200

    mmc: core: Delete error messages for failed memory allocations
    Omit an extra message for memory allocation failures.
    
    This issue was detected by using the Coccinelle software.
    
    Link: http://events.linuxfoundation.org/sites/events/files/slides/LCJ16-Refactor_Strings-WSang_0.pdf
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/mmc/card/mmc_test.c
+++ drivers/mmc/card/mmc_test.c
@@ -2887,8 +2887,6 @@ static int __mmc_test_register_dbgfs_file(struct mmc_card *card,
 	df = kmalloc(sizeof(struct mmc_test_dbgfs_file), GFP_KERNEL);
 	if (!df) {
 		debugfs_remove(file);
-		dev_err(&card->dev,
-			"Can't allocate memory for internal usage.\n");
 		return -ENOMEM;
 	}
 
