35589f0901e304e7a64b2da0198be2ef0c15e26a
Wanpeng Li <wanpeng.li@hotmail.com>
2017-07-09 00:40:28 -0700

    sched/cputime: Don't use smp_processor_id() in preemptible context
    Recent kernels trigger this warning:
    
     BUG: using smp_processor_id() in preemptible [00000000] code: 99-trinity/181
     caller is debug_smp_processor_id+0x17/0x19
     CPU: 0 PID: 181 Comm: 99-trinity Not tainted 4.12.0-01059-g2a42eb9 #1
     Call Trace:
      dump_stack+0x82/0xb8
      check_preemption_disabled()
      debug_smp_processor_id()
      vtime_delta()
      task_cputime()
      thread_group_cputime()
      thread_group_cputime_adjusted()
      wait_consider_task()
      do_wait()
      SYSC_wait4()
      do_syscall_64()
      entry_SYSCALL64_slow_path()
    
    As Frederic pointed out:
    
    | Although those sched_clock_cpu() things seem to only matter when the
    | sched_clock() is unstable. And that stability is a condition for nohz_full
    | to work anyway. So probably sched_clock() alone would be enough.
    
    This patch fixes it by replacing sched_clock_cpu() with sched_clock() to
    avoid calling smp_processor_id() in a preemptible context.
    
    Reported-by: Xiaolong Ye <xiaolong.ye@intel.com>
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Luiz Capitulino <lcapitulino@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1499586028-7402-1-git-send-email-wanpeng.li@hotmail.com
    [ Prettified the changelog. ]
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -722,7 +722,7 @@ static u64 vtime_delta(struct vtime *vtime)
 {
 	unsigned long long clock;
 
-	clock = sched_clock_cpu(smp_processor_id());
+	clock = sched_clock();
 	if (clock < vtime->starttime)
 		return 0;
 
@@ -853,7 +853,7 @@ void arch_vtime_task_switch(struct task_struct *prev)
 
 	write_seqcount_begin(&vtime->seqcount);
 	vtime->state = VTIME_SYS;
-	vtime->starttime = sched_clock_cpu(smp_processor_id());
+	vtime->starttime = sched_clock();
 	write_seqcount_end(&vtime->seqcount);
 }
 
@@ -865,7 +865,7 @@ void vtime_init_idle(struct task_struct *t, int cpu)
 	local_irq_save(flags);
 	write_seqcount_begin(&vtime->seqcount);
 	vtime->state = VTIME_SYS;
-	vtime->starttime = sched_clock_cpu(cpu);
+	vtime->starttime = sched_clock();
 	write_seqcount_end(&vtime->seqcount);
 	local_irq_restore(flags);
 }
