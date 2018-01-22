3469ffdb85e924541bebe5d985558075bd24cbd7
Tejun Heo <tj@kernel.org>
2018-01-22 14:00:55 -0800

    locking/lockdep: Avoid triggering hardlockup from debug_show_all_locks()
    debug_show_all_locks() iterates all tasks and print held locks whole
    holding tasklist_lock.  This can take a while on a slow console device
    and may end up triggering NMI hardlockup detector if someone else ends
    up waiting for tasklist_lock.
    
    Touch the NMI watchdog while printing the held locks to avoid
    spuriously triggering the hardlockup detector.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: kernel-team@fb.com
    Link: http://lkml.kernel.org/r/20180122220055.GB1771050@devbig577.frc2.facebook.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -47,6 +47,7 @@
 #include <linux/gfp.h>
 #include <linux/kmemcheck.h>
 #include <linux/jhash.h>
+#include <linux/nmi.h>
 
 #include <asm/sections.h>
 
@@ -4533,6 +4534,7 @@ retry:
 		if (!unlock)
 			if (read_trylock(&tasklist_lock))
 				unlock = 1;
+		touch_nmi_watchdog();
 	} while_each_thread(g, p);
 
 	pr_warn("\n");
