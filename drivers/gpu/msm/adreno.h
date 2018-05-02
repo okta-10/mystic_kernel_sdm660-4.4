c6c6ce283a7940f625f36b72cb5e622f20fd26a5
Rajesh Kemisetti <rajeshk@codeaurora.org>
2018-05-02 15:32:38 +0530

    msm: kgsl: Try again if GPU zap shader loading fails
    Sometimes GPU Zap shader loading might fail with -EAGAIN.
    Try to load again in such conditions to see if it succeeds.
    
    Change-Id: Icd8e582c36f8f19ff765ed65c2ff68fe6bb007bf
    Signed-off-by: Rajesh Kemisetti <rajeshk@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -144,6 +144,9 @@
 #define KGSL_END_OF_PROFILE_IDENTIFIER	0x2DEFADE2
 #define KGSL_PWRON_FIXUP_IDENTIFIER	0x2AFAFAFA
 
+/* Number of times to try loading of zap shader */
+#define ZAP_RETRY_MAX	5
+
 /* One cannot wait forever for the core to idle, so set an upper limit to the
  * amount of time to wait for the core to go idle
  */
