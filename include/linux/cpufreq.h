a390447dbd3fce4490bd8b753f477871795bb5d6
feudofjustice <brandanhenderson07@gmail.com>
2018-08-06 18:43:30 -0400

    cpufreq:Introduce Darkutil-a combination of Helix + Electroutil.
    squash :
    cpufreq_darkutil: squashed update
    Added the changes from: https://github.com/holyangel/dm/blob/hdo/kernel/sched/cpufreq_darkutil.c
    Last commit: Further changes to darkutil. 9c92fbcabe10b647a5d076007ae9d8f2dd988e67
    cpufreq: darkutil: squashed update
    List of commits:
    cpufreq: schedutil: Use >= when aggregating CPU loads in a policy
    cpufreq: schedutil: Fix sugov_start versus sugov_update_shared race
    BACKPORT: cpufreq: Return 0 from ->fast_switch() on errors
    cpufreq: schedutil: Fix iowait boost reset
    schedutil: Allow cpufreq requests to be made even when kthread kicked
    cpufreq: schedutil: Don't set next_freq to UINT_MAX
    BACKPORT: cpufreq: schedutil: Avoid using invalid next_freq
    cpufreq: schedutil: Ignore CPU load older than WALT window size
    cpufreq: schedutil: Avoid unnecessary kthread wakeup
    cpufreq: schedutil: Switch from sprintf to scnprintf
    FIXUP: cpufreq: schedutil: Don't set next_freq to UINT_MAX
    BACKPORT: cpufreq: schedutil: clear cached_raw_freq when invalidated

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -543,7 +543,11 @@ extern struct cpufreq_governor cpufreq_gov_schedutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKUTIL)
+extern struct cpufreq_governor cpufreq_gov_darkutil;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkutil)
 #endif
+
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
 {
