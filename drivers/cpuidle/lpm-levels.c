08d96971d9719af7706febe5269f0294dc8dd1ec
Yaroslav Furman <yaro330@gmail.com>
2018-08-18 20:45:15 +0300

    BACKPORT: lpm-levels: Add support to parse LPM parameters
    Add support for providing LPM prediction parameters in
    device tree and parsing them.
    
    Change-Id: I60fd7e8d32505e212100b886df29b72a9999ca6d
    Signed-off-by: Raghavendra Kakarla <rkakarla@codeaurora.org>
    [yaro] Backported to msm8998.
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -71,18 +71,6 @@ static bool lpm_prediction = true;
 module_param_named(lpm_prediction,
 	lpm_prediction, bool, S_IRUGO | S_IWUSR | S_IWGRP);
 
-static uint32_t ref_stddev = 500;
-module_param_named(
-	ref_stddev, ref_stddev, uint, S_IRUGO | S_IWUSR | S_IWGRP
-);
-
-static uint32_t tmr_add = 1000;
-module_param_named(
-	tmr_add, tmr_add, uint, S_IRUGO | S_IWUSR | S_IWGRP
-);
-
-static uint32_t ref_premature_cnt = 1;
-
 struct lpm_history {
 	uint32_t resi[MAXSAMPLES];
 	int mode[MAXSAMPLES];
@@ -580,7 +568,7 @@ again:
 	 * ignore one maximum sample and retry
 	 */
 	if (((avg > stddev * 6) && (divisor >= (MAXSAMPLES - 1)))
-					|| stddev <= ref_stddev) {
+					|| stddev <= cpu->ref_stddev) {
 		history->stime = ktime_to_us(ktime_get()) + avg;
 		return avg;
 	} else if (divisor  > (MAXSAMPLES - 1)) {
@@ -605,7 +593,8 @@ again:
 					total += history->resi[i];
 				}
 			}
-			if (failed >= ref_premature_cnt) {
+
+			if (failed >= cpu->ref_premature_cnt) {
 				*idx_restrict = j;
 				do_div(total, failed);
 				for (i = 0; i < j; i++) {
@@ -755,8 +744,8 @@ static int cpu_power_select(struct cpuidle_device *dev,
 	if ((predicted || (idx_restrict != (cpu->nlevels + 1)))
 			&& ((best_level >= 0)
 			&& (best_level < (cpu->nlevels-1)))) {
-		htime = predicted + tmr_add;
-		if (htime == tmr_add)
+		htime = predicted + cpu->tmr_add;
+		if (htime == cpu->tmr_add)
 			htime = idx_restrict_time;
 		else if (htime > max_residency[best_level])
 			htime = max_residency[best_level];
@@ -1162,7 +1151,9 @@ static int cluster_configure(struct lpm_cluster *cluster, int idx,
 		struct power_params *pwr_params = &cluster->levels[idx].pwr;
 
 		tick_broadcast_exit();
-		clusttimer_start(cluster, pwr_params->max_residency + tmr_add);
+		clusttimer_start(cluster,
+						 pwr_params->max_residency +
+						 cluster->tmr_add);
 		tick_broadcast_enter();
 	}
 
@@ -1228,7 +1219,8 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 
 			tick_broadcast_exit();
 			clusttimer_start(cluster,
-					pwr_params->max_residency + tmr_add);
+					pwr_params->max_residency +
+							 cluster->tmr_add);
 			tick_broadcast_enter();
 		}
 	}
