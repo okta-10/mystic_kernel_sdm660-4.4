2f98452a6bf8722092a872fa5c813a2ced46434c
ktommy91 <kemenesi.tamas@gmail.com>
2018-09-04 02:23:52 +0200

    sdm660_audio: use 24.576MHz MCLK
    Signed-off-by: ktommy91 <kemenesi.tamas@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- sound/soc/msm/sdm660-common.c
+++ sound/soc/msm/sdm660-common.c
@@ -31,7 +31,7 @@
 #define PMIC_INT_ANALOG_CODEC "analog-codec"
 
 #define DEV_NAME_STR_LEN  32
-#define DEFAULT_MCLK_RATE 9600000
+#define DEFAULT_MCLK_RATE 24576000
 #define MSM_LL_QOS_VALUE 300 /* time in us to ensure LPM doesn't go in C3/C4 */
 
 struct dev_config {
