fa27783484a2214ce2eed39e3f7a24f15877b828
Nathan Chancellor <natechancellor@gmail.com>
2018-09-21 20:27:35 -0700

    Remove Qualcomm's panic changes
    There is an upstream version of this that we'll be using.
    
    Reverts the following commits:
    
    01ee295a4adf ("lib: spinlock: Change MSM_WATCHDOG_V2 to QCOM_WATCHDOG_V2")
    774b4c9f4aad ("lib: Kconfig.debug: Fix the recursive dependency")
    805c18d71c61 ("lib: spinlock: Trigger a watchdog bite on spin_dump for rwlock")
    77d758e28363 ("lib: spinlock: Cause a watchdog bite on spin_dump")
    e2cddd104051 ("kernel/lib: add additional debug capabilites for data corruption")
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- lib/list_debug.c
+++ lib/list_debug.c
@@ -10,7 +10,6 @@
 #include <linux/bug.h>
 #include <linux/kernel.h>
 #include <linux/rculist.h>
-#include <linux/bug.h>
 
 /*
  * Check that the data structures for the list manipulations are reasonably
@@ -21,18 +20,21 @@
 bool __list_add_valid(struct list_head *new, struct list_head *prev,
 		      struct list_head *next)
 {
-	if (CHECK_DATA_CORRUPTION(next->prev != prev,
-			"list_add corruption. next->prev should be prev (%p), but was %p. (next=%p).\n",
-			prev, next->prev, next) ||
-	    CHECK_DATA_CORRUPTION(prev->next != next,
-			"list_add corruption. prev->next should be next (%p), but was %p. (prev=%p).\n",
-			next, prev->next, prev) ||
-	    CHECK_DATA_CORRUPTION(new == prev || new == next,
-			"list_add double add: new=%p, prev=%p, next=%p.\n",
-			new, prev, next))
-		return false;
-
-	return true;
+	WARN(next->prev != prev,
+		"list_add corruption. next->prev should be "
+		"prev (%p), but was %p. (next=%p).\n",
+		prev, next->prev, next);
+	WARN(prev->next != next,
+		"list_add corruption. prev->next should be "
+		"next (%p), but was %p. (prev=%p).\n",
+		next, prev->next, prev);
+	WARN(new == prev || new == next,
+	     "list_add double add: new=%p, prev=%p, next=%p.\n",
+	     new, prev, next);
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	prev->next = new;
 }
 EXPORT_SYMBOL(__list_add_valid);
 
@@ -43,21 +45,37 @@ bool __list_del_entry_valid(struct list_head *entry)
 	prev = entry->prev;
 	next = entry->next;
 
-	if (CHECK_DATA_CORRUPTION(next == LIST_POISON1,
-			"list_del corruption, %p->next is LIST_POISON1 (%p)\n",
-			entry, LIST_POISON1) ||
-	    CHECK_DATA_CORRUPTION(prev == LIST_POISON2,
-			"list_del corruption, %p->prev is LIST_POISON2 (%p)\n",
-			entry, LIST_POISON2) ||
-	    CHECK_DATA_CORRUPTION(prev->next != entry,
-			"list_del corruption. prev->next should be %p, but was %p\n",
-			entry, prev->next) ||
-	    CHECK_DATA_CORRUPTION(next->prev != entry,
-			"list_del corruption. next->prev should be %p, but was %p\n",
-			entry, next->prev))
-		return false;
+	if (WARN(next == LIST_POISON1,
+		"list_del corruption, %p->next is LIST_POISON1 (%p)\n",
+		entry, LIST_POISON1) ||
+	    WARN(prev == LIST_POISON2,
+		"list_del corruption, %p->prev is LIST_POISON2 (%p)\n",
+		entry, LIST_POISON2) ||
+	    WARN(prev->next != entry,
+		"list_del corruption. prev->next should be %p, "
+		"but was %p\n", entry, prev->next) ||
+	    WARN(next->prev != entry,
+		"list_del corruption. next->prev should be %p, "
+		"but was %p\n", entry, next->prev))
+		return;
 
-	return true;
+	__list_del(prev, next);
+}
+EXPORT_SYMBOL(__list_del_entry);
+
+/**
+ * list_del - deletes entry from list.
+ * @entry: the element to delete from the list.
+ * Note: list_empty on entry does not return true after this, the entry is
+ * in an undefined state.
+ */
+void list_del(struct list_head *entry)
+{
+	__list_del_entry(entry);
+	entry->next = LIST_POISON1;
+	entry->prev = LIST_POISON2;
+}
+EXPORT_SYMBOL(list_del);
 
 }
 EXPORT_SYMBOL(__list_del_entry_valid);
