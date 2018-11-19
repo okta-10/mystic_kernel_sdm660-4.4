8073271b67e68bd141f506413dc9e087d5d9d2e3
Neal Cardwell <ncardwell@google.com>
2018-11-19 13:48:36 -0500

    FROMGIT: net-tcp_bbr: v2: export FLAG_ECE in rate_sample.is_ece
    For understanding the relationship between inflight and ECN signals,
    to try to find the highest inflight value that has acceptable levels
    ECN marking.
    
    Effort: net-tcp_bbr
    Origin-9xx-SHA1: 3eba998f2898541406c2666781182200934965a8
    Change-Id: I3a964e04cee83e11649a54507043d2dfe769a3b3
    (cherry-picked from https://github.com/google/bbr/commit/d51b05c0f03000317360f529f926d58d4f17fb07)
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -3726,6 +3726,7 @@ static int tcp_ack(struct sock *sk, const struct sk_buff *skb, int flag)
 	lost = tp->lost - lost;			/* freshly marked lost */
 	tcp_rate_gen(sk, delivered, lost, sack_state.rate);
 	rs.is_ack_delayed = !!(flag & FLAG_ACK_MAYBE_DELAYED);
+	rs.is_ece = !!(flag & FLAG_ECE);
 	tcp_cong_control(sk, ack, delivered, flag, sack_state.rate);
 	tcp_xmit_recovery(sk, rexmit);
 	return 1;
