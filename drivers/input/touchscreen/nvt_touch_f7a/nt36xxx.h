bc55987034486e74b29131638f105f3b0b98c8ad
Tyler Nijmeh <tylernij@gmail.com>
2019-02-13 13:34:13 -0800

    nvt_touch_f7a: Remove earlysuspend support
    Truthfully, hardly any modern devices support earlysuspend anymore
    instead of FB. We can just remove this filler code.
    
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.h
+++ drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.h
@@ -22,9 +22,6 @@
 #include <linux/i2c.h>
 #include <linux/input.h>
 #include <linux/regulator/consumer.h>
-#ifdef CONFIG_HAS_EARLYSUSPEND
-#include <linux/earlysuspend.h>
-#endif
 
 #include "nt36xxx_mem_map.h"
 
@@ -119,11 +116,7 @@ struct nvt_ts_data {
 	struct delayed_work nvt_fwu_work;
 	uint16_t addr;
 	int8_t phys[32];
-#if defined(CONFIG_FB)
 	struct notifier_block fb_notif;
-#elif defined(CONFIG_HAS_EARLYSUSPEND)
-	struct early_suspend early_suspend;
-#endif
 	struct regulator *vcc_i2c;
 	uint8_t fw_ver;
 	uint8_t x_num;
