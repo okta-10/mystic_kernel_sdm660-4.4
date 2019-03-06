d60ede58395bcb78ed8c28e3b17af26a574aae85
flar2 <asegaert@gmail.com>
2019-03-05 21:45:27 -0500

    KLAPSE: adapt for OP5/5T

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -60,6 +60,9 @@
 #include "mdss_mdp.h"
 #include "dsi_access.h"
 
+#ifdef CONFIG_KLAPSE
+#include "klapse.h"
+#endif
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
@@ -362,6 +365,9 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 	}
 	mfd->bl_level_usr = bl_lvl;
 
+#ifdef CONFIG_KLAPSE
+	set_rgb_slider(bl_lvl);
+#endif
 }
 
 static enum led_brightness mdss_fb_get_bl_brightness(
