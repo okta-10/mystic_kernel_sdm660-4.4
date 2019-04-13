e1abffd527279cf7529bd2386a8128c3347179a1
Park Ju Hyung <qkrwngud825@gmail.com>
2019-04-13 19:00:11 +0900

    dsp: asm: improve misleading logs
    On case ASM_STREAM_CMD_OPEN_WRITE_COMPRESSED, payload size error log
    might be shown when it's actually correct due to (payload[1] != 0)
    check failing. Fix this.
    
    Also, add missing new line and change size error logs to debug type
    if checks are done solely to print debug messages.
    
    Commit 979f3d57b439 ("dsp: asm: validate payload size before access")
    introduced these payload size check logs.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -2024,9 +2024,11 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 		case ASM_DATA_CMD_REMOVE_TRAILING_SILENCE:
 		case ASM_SESSION_CMD_REGISTER_FOR_RX_UNDERFLOW_EVENTS:
 		case ASM_STREAM_CMD_OPEN_WRITE_COMPRESSED:
-			if (data->payload_size >=
-				2 * sizeof(uint32_t) &&
-				payload[1] != 0) {
+			if (data->payload_size <
+				2 * sizeof(uint32_t)) {
+				pr_err("%s: payload size of %x is less than expected.\n",
+					__func__, data->payload_size);
+			} else if (payload[1] != 0) {
 				pr_debug("%s: session %d opcode 0x%x token 0x%x Payload = [0x%x] stat 0x%x src %d dest %d\n",
 					__func__, ac->session,
 					data->opcode, data->token,
@@ -2055,9 +2057,6 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 					&(session[session_id].session_lock),
 					flags);
 				return 0;
-			} else {
-				pr_debug("%s: payload size of %x is less than expected.\n",
-					__func__, data->payload_size);
 			}
 			if ((is_adsp_reg_event(payload[0]) >= 0) ||
 			    (payload[0] == ASM_STREAM_CMD_SET_PP_PARAMS_V2) ||
@@ -2240,7 +2239,7 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 							payload[2],
 							payload[3]);
 				else
-					pr_debug("%s: payload size of %x is less than expected.\n",
+					pr_debug("%s: payload size of %u is less than expected.\n",
 						__func__,
 						data->payload_size);
 
@@ -2363,7 +2362,7 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 				(uint64_t)(((uint64_t)payload[2] << 32) |
 					payload[1]);
 		} else {
-			dev_vdbg(ac->dev, "%s: payload size of %x is less than expected.n",
+			dev_err(ac->dev, "%s: payload size of %x is less than expected.\n",
 				__func__, data->payload_size);
 		}
 		if (atomic_cmpxchg(&ac->time_flag, 1, 0))
