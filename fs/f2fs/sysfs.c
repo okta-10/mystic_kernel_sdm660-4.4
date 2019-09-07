72e6dbd0fa7f0dad91ef035136f02af745915079
celtare21 <celtare21@gmail.com>
2019-09-07 08:10:00 +0000

    f2fs: Set cp_interval to 30 secs and make it no-op
    https://github.com/celtare21/kernel_essential_msm8998-1/commit/09a58e0190318a8ba606d36017a7ab1bb2205c8e made dirty_expire_centisecs 30 secs, cp_interval should be
    at >= than dirty_expire_centisecs so adjust the value for it in kernel
    and make it no-op so userspace doesn't overwrite it with other values.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -399,7 +399,6 @@ F2FS_RW_ATTR(NM_INFO, f2fs_nm_info, dirty_nats_ratio, dirty_nats_ratio);
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, max_victim_search, max_victim_search);
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, migration_granularity, migration_granularity);
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, dir_level, dir_level);
-F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, cp_interval, interval_time[CP_TIME]);
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, idle_interval, interval_time[REQ_TIME]);
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, discard_idle_interval,
 					interval_time[DISCARD_TIME]);
@@ -453,7 +452,6 @@ static struct attribute *f2fs_attrs[] = {
 	ATTR_LIST(ram_thresh),
 	ATTR_LIST(ra_nid_pages),
 	ATTR_LIST(dirty_nats_ratio),
-	ATTR_LIST(cp_interval),
 	ATTR_LIST(idle_interval),
 	ATTR_LIST(discard_idle_interval),
 	ATTR_LIST(umount_discard_timeout),
