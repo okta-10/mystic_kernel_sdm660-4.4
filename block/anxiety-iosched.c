042313bdf3117e140ef4dad1cae08c95a636b14a
Tyler Nijmeh <tylernij@gmail.com>
2019-09-11 20:40:19 -0700

    anxiety-iosched: Update GPL license header to SPDX
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- block/anxiety-iosched.c
+++ block/anxiety-iosched.c
@@ -1,14 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2019, Tyler Nijmeh <tylernij@gmail.com>.
+ * Anxiety I/O Scheduler
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (c) 2019, Tyler Nijmeh <tylernij@gmail.com>
  */
 
 #include <linux/blkdev.h>
