06a5534ab8b8d517828804cb3ab20e7ca6d040c9
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-10-05 17:05:46 +0530

    mdss: boost devfreq only if the last input was within 3250ms
    based off https://github.com/kdrag0n/proton_bluecross/commit/73171d86911fb8ae50ba201eb1eb25a6dab1afc8
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -5877,7 +5877,9 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		break;
 	case MSMFB_ATOMIC_COMMIT:
 		cpu_input_boost_kick();
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		if (time_before(jiffies, last_input_jiffies + msecs_to_jiffies(3250))) {
+			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		}
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
