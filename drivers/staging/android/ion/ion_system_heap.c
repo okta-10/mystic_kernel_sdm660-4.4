fad18c7d53f0931397b7b7c590e71cb8797ff31c
Sultan Alsawaf <sultan@kerneltoast.com>
2019-10-19 13:22:36 -0700

    ion: system_heap: Fix uninitialized sg-table usage
    The table_sync sg-table is used uninitialized when nents_sync is zero.
    Fix it by only using it when it's allocated.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -426,11 +426,13 @@ static int ion_system_heap_allocate(struct ion_heap *heap,
 		ret = sg_alloc_table(&table_sync, nents_sync, GFP_KERNEL);
 		if (ret)
 			goto err_free_sg;
+		sg_sync = table_sync.sgl;
+	} else {
+		sg_sync = NULL;
 	}
 
 	i = 0;
 	sg = table->sgl;
-	sg_sync = table_sync.sgl;
 
 	/*
 	 * We now have two separate lists. One list contains pages from the
@@ -445,13 +447,15 @@ static int ion_system_heap_allocate(struct ion_heap *heap,
 		if (info && tmp_info) {
 			if (info->order >= tmp_info->order) {
 				i = process_info(info, sg, sg_sync, &data, i);
-				sg_sync = sg_next(sg_sync);
+				if (sg_sync)
+					sg_sync = sg_next(sg_sync);
 			} else {
 				i = process_info(tmp_info, sg, 0, 0, i);
 			}
 		} else if (info) {
 			i = process_info(info, sg, sg_sync, &data, i);
-			sg_sync = sg_next(sg_sync);
+			if (sg_sync)
+				sg_sync = sg_next(sg_sync);
 		} else if (tmp_info) {
 			i = process_info(tmp_info, sg, 0, 0, i);
 		} else {
