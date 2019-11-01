bfc8b1e00d59510a697ed293f15d532721a3a544
Oktapra Amtono <oktapra.amtono@gmail.com>
2019-11-01 12:51:11 +0700

    fs: fix error

--- fs/readdir.c
+++ fs/readdir.c
@@ -54,7 +54,7 @@ EXPORT_SYMBOL(iterate_dir);
 static bool hide_name(const char *name, int namlen)
 {
 	if (namlen == 2 && !memcmp(name, "su", 2))
-		if (!su_visible())
+		// if (!su_visible())
 			return true;
 	return false;
 }
