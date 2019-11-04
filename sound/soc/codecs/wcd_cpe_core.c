3a85e8a9ad294b1b78228a918b2a9bb029b87cbd
Jean-Baptiste Théou <jb@essential.com>
2019-11-05 07:36:56 +0900

    wcd_cpe: fix buffer size
    The latest Clang highlight this buffer size issue:
    
    sound/soc/codecs/wcd_cpe_core.c:1981:2: error: 'snprintf' size argument
    is too large; destination buffer has size 11, but size argument is 13
    [-Werror,-Wfortify-source]
            snprintf(proc_name, (sizeof("cpe") + sizeof("_state") +
    
    Fixed by adjusting the buffer size
    
    Change-Id: Ica51297b39f89bec1a5e4aed4cd295e0bdd622d9
    Signed-off-by: Jean-Baptiste Théou <jb@essential.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- sound/soc/codecs/wcd_cpe_core.c
+++ sound/soc/codecs/wcd_cpe_core.c
@@ -60,7 +60,7 @@
 	mutex_unlock(lock);			\
 }
 
-#define WCD_CPE_STATE_MAX_LEN 11
+#define WCD_CPE_STATE_MAX_LEN 13
 #define CPE_OFFLINE_WAIT_TIMEOUT msecs_to_jiffies(200)
 #define CPE_READY_WAIT_TIMEOUT msecs_to_jiffies(300)
 #define WCD_CPE_SYSFS_DIR_MAX_LENGTH 32
