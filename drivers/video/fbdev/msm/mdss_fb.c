f5ba6f74e1873c40f0687b5976827bda25249ab9
Oktapra Amtono <oktapra.amtono@gmail.com>
2019-11-27 12:21:30 +0700

    KLAPSE: Remove

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -60,9 +60,6 @@
 #include "mdss_mdp.h"
 #include "dsi_access.h"
 
-#ifdef CONFIG_KLAPSE
-#include "klapse.h"
-#endif
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
@@ -365,9 +362,6 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 	}
 	mfd->bl_level_usr = bl_lvl;
 
-#ifdef CONFIG_KLAPSE
-	set_rgb_slider(bl_lvl);
-#endif
 }
 
 static enum led_brightness mdss_fb_get_bl_brightness(
