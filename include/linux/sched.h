f25d2870f5375e7f0792401126aab92598bbcce1
Oktapra Amtono <oktapra.amtono@gmail.com>
2019-12-01 12:49:50 +0700

    Revert "sched/tune: Expose sched_boost"
    This reverts commit aa129ea3608eebd024a189cd1e49771c13017bc4.
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3592,8 +3592,6 @@ void cpufreq_remove_update_util_hook(int cpu);
 int do_stune_boost(char *st_name, int boost, int *slot);
 int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
-
-int get_sched_boost(char *st_name);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 #endif
