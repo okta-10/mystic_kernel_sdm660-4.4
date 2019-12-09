c2cb14f32718ef5d8454282374c5e15601bcf2b3
Oktapra Amtono <oktapra.amtono@gmail.com>
2019-12-09 11:55:58 +0700

    sched/cpufreq: Add helix_schedutil governor
    diff from 'https://github.com/kdrag0n/velocity_tissot/commit/1b8e8b5e8f3ad15307903080ff202105d2428bb4'

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -549,6 +549,9 @@ extern struct cpufreq_governor cpufreq_gov_darkutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTROUTIL)
 extern struct cpufreq_governor cpufreq_gov_electroutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electroutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HELIX)
+extern struct cpufreq_governor cpufreq_gov_helix_schedutil;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_helix_schedutil)
 #endif
 
 
