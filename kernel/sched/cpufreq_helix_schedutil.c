2f1bd6f125477d8391b3fd19f37b1a56c28de11b
Oktapra Amtono <oktapra.amtono@gmail.com>
2019-12-09 12:24:36 +0700

    sched/cpufreq: cpufreq_helix_schedutil: fix error
    ../kernel/sched/cpufreq_helix_schedutil.c:367:27: warning: unused variable 'j_cap_min' [-Wunused-variable]
       unsigned int j_cap_max, j_cap_min;
                               ^
    ../kernel/sched/cpufreq_helix_schedutil.c:367:16: warning: unused variable 'j_cap_max' [-Wunused-variable]
       unsigned int j_cap_max, j_cap_min;
                    ^
    ../kernel/sched/cpufreq_helix_schedutil.c:356:15: warning: unused variable 'cap_min' [-Wunused-variable]
      unsigned int cap_min = 0;
                   ^
    ../kernel/sched/cpufreq_helix_schedutil.c:355:15: warning: unused variable 'cap_max' [-Wunused-variable]
      unsigned int cap_max = SCHED_CAPACITY_SCALE;
                   ^
    ../kernel/sched/cpufreq_helix_schedutil.c: In function 'hxgov_update_shared':
    ../kernel/sched/cpufreq_helix_schedutil.c:428:14: error: 'SCHED_CPUFREQ_IDLE' undeclared (first use in this function)
      if (flags & SCHED_CPUFREQ_IDLE) {
                  ^

--- kernel/sched/cpufreq_helix_schedutil.c
+++ kernel/sched/cpufreq_helix_schedutil.c
@@ -352,8 +352,6 @@ static unsigned int hxgov_next_freq_shared(struct hxgov_cpu *sg_cpu,
 	struct hxgov_policy *sg_policy = sg_cpu->sg_policy;
 	struct cpufreq_policy *policy = sg_policy->policy;
 	u64 last_freq_update_time = sg_policy->last_freq_update_time;
-	unsigned int cap_max = SCHED_CAPACITY_SCALE;
-	unsigned int cap_min = 0;
 	unsigned int j;
 
 	hxgov_iowait_boost(sg_cpu, &util, &max);
@@ -364,7 +362,6 @@ static unsigned int hxgov_next_freq_shared(struct hxgov_cpu *sg_cpu,
 	for_each_cpu(j, policy->cpus) {
 		struct hxgov_cpu *j_sg_cpu;
 		unsigned long j_util, j_max;
-		unsigned int j_cap_max, j_cap_min;
 		s64 delta_ns;
 
 		if (j == smp_processor_id())
@@ -425,7 +422,7 @@ static void hxgov_update_shared(struct update_util_data *hook, u64 time,
 	raw_spin_lock(&sg_policy->update_lock);
 
 	/* CPU is entering IDLE, reset flags without triggering an update */
-	if (flags & SCHED_CPUFREQ_IDLE) {
+	if (flags & SCHED_IDLE) {
 		sg_cpu->flags = 0;
 		goto done;
 	}
