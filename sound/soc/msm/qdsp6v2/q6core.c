167d9c14cfcdab57c5d95626e1d69645f9c0cc66
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-01-05 09:40:48 +0700

    Revert "sound: qdsp6v2: Make version checking no-op"
    This reverts commit bfa7631eadc0269888050d1139da63f58a2a10f6.

--- sound/soc/msm/qdsp6v2/q6core.c
+++ sound/soc/msm/qdsp6v2/q6core.c
@@ -375,7 +375,6 @@ struct cal_block_data *cal_utils_get_cal_block_by_key(
 	return NULL;
 }
 
-#if 0
 static int q6core_send_get_avcs_fwk_ver_cmd(void)
 {
 	struct apr_hdr avcs_ver_cmd;
@@ -477,6 +476,7 @@ int q6core_get_service_version(uint32_t service_id,
 	pr_err("%s: No service matching service ID %d\n", __func__, service_id);
 	return -EINVAL;
 }
+EXPORT_SYMBOL(q6core_get_service_version);
 
 size_t q6core_get_fwk_version_size(uint32_t service_id)
 {
@@ -533,20 +533,6 @@ size_t q6core_get_fwk_version_size(uint32_t service_id)
 done:
 	return ret;
 }
-#else
-int q6core_get_service_version(uint32_t service_id,
-			       struct avcs_fwk_ver_info *ver_info,
-			       size_t size)
-{
-	return -1;
-}
-
-size_t q6core_get_fwk_version_size(uint32_t service_id)
-{
-	return -1;
-}
-#endif
-EXPORT_SYMBOL(q6core_get_service_version);
 EXPORT_SYMBOL(q6core_get_fwk_version_size);
 
 int32_t core_set_license(uint32_t key, uint32_t module_id)
