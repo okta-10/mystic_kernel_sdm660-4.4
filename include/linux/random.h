e19896d0909daf89bb3803f40e3dfcd7ebd970d4
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-02-15 15:49:44 +0700

    treewide: remove error duplicate
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- include/linux/random.h
+++ include/linux/random.h
@@ -34,8 +34,6 @@ extern void add_input_randomness(unsigned int type, unsigned int code,
 extern void add_interrupt_randomness(int irq, int irq_flags);
 
 extern void get_random_bytes(void *buf, int nbytes);
-extern int wait_for_random_bytes(void);
-extern bool rng_is_initialized(void);
 extern int add_random_ready_callback(struct random_ready_callback *rdy);
 extern void del_random_ready_callback(struct random_ready_callback *rdy);
 extern int __must_check get_random_bytes_arch(void *buf, int nbytes);
