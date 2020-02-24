922fe4f3128f739f5cbe3bfa8500f814feb1616d
dianlujitao <dianlujitao@lineageos.org>
2020-02-24 19:56:02 +0100

    input: nvt_touch_a: Implement double_tap node
    Change-Id: Id69b7cdb7ba55f6fc0de8a0a3a5ebc5b49a3e8a0
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/input/touchscreen/nvt_touch_a/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_a/nt36xxx.c
@@ -76,6 +76,9 @@ static int fb_notifier_callback(struct notifier_block *self, unsigned long event
 static void nvt_ts_early_suspend(struct early_suspend *h);
 static void nvt_ts_late_resume(struct early_suspend *h);
 #endif
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+#include <linux/input/tp_common.h>
+#endif
 
 #if TOUCH_KEY_NUM > 0
 const uint16_t touch_key_array[TOUCH_KEY_NUM] = {
@@ -159,6 +162,33 @@ static const struct file_operations nvt_gesture_fops = {
 	.read = seq_read,
 	.llseek = seq_lseek,
 };
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+			       struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", enable_gesture_mode);
+}
+
+static ssize_t double_tap_store(struct kobject *kobj,
+				struct kobj_attribute *attr, const char *buf,
+				size_t count)
+{
+	int rc, val;
+
+	rc = kstrtoint(buf, 10, &val);
+	if (rc)
+		return -EINVAL;
+
+	enable_gesture_mode = !!val;
+	return count;
+}
+
+static struct tp_common_ops double_tap_ops = {
+	.show = double_tap_show,
+	.store = double_tap_store
+};
+#endif
 
 #endif
 
@@ -1379,6 +1409,13 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 		NVT_ERR("error while create gesture");
 
 	wake_lock_init(&gestrue_wakelock, WAKE_LOCK_SUSPEND, "poll-wake-lock");
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+	ret = tp_common_set_double_tap_ops(&double_tap_ops);
+	if (ret < 0) {
+		NVT_ERR("%s: Failed to create double_tap node err=%d\n",
+			__func__, ret);
+	}
+#endif
 #endif
 
 	sprintf(ts->phys, "input/ts");
