31bd2b30197a62f296dcb41d2a3b403160665bd7
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-03-10 13:39:42 +0700

    cpufreq_schedutil: fix error compile
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -356,7 +356,6 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 			/* clear cache when it's bypassed */
 			sg_policy->cached_raw_freq = 0;
 		}
-	}
 	sugov_update_commit(sg_policy, time, next_f);
 }
 
