de61a53343dc81a2fa9ab7695a8a07ccebdd7d7e
theradcolor <theradcolor@gmail.com>
2020-03-15 22:15:00 +0700

    drivers: msm: camera_v2: Make unified for Newer and older camera blobs
    This patch add supports for both old as well as Xiaomi's
    new camera blobs in our kernel, Make it configure with
    CONFIG_XIAOMI_NEW_CAMERA_BLOBS in device configuration
    Say "y" for support with new camera blobs and "n" for old camera blobs.
    
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -1447,7 +1447,9 @@ int32_t msm_sensor_driver_probe(void *setting,
 		slave_info->sensor_id_info = slave_info32->sensor_id_info;
 		slave_info->vendor_id_info = slave_info32->vendor_id_info;
 		slave_info->vcm_id_info = slave_info32->vcm_id_info;
+#ifdef CONFIG_XIAOMI_NEW_CAMERA_BLOBS
 		slave_info->lens_id_info = slave_info32->lens_id_info;
+#endif
 
 		slave_info->slave_addr = slave_info32->slave_addr;
 		slave_info->power_setting_array.size =
@@ -1718,7 +1720,9 @@ CSID_TG:
 	s_ctrl->sensordata->flash_name = slave_info->flash_name;
 	s_ctrl->sensordata->vendor_id_info = &(slave_info->vendor_id_info);
 	s_ctrl->sensordata->vcm_id_info = &(slave_info->vcm_id_info);
+#ifdef CONFIG_XIAOMI_NEW_CAMERA_BLOBS
 	s_ctrl->sensordata->lens_id_info = &(slave_info->lens_id_info);
+#endif
 
 	/*
 	 * Update eeporm subdevice Id by input eeprom name
