edf927d238d5ef5f6e03c31363237d19cce3a3b8
psndna88 <psndna88@gmail.com>
2020-04-03 13:45:41 +0530

    disable tracing/logging at various places Part 7
    Signed-off-by: Parvinder Singh <psndna88@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6883,10 +6883,10 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					schedstat_inc(p, se.statistics.nr_wakeups_fbt_pref_idle);
 					schedstat_inc(this_rq(), eas_stats.fbt_pref_idle);
 
-					trace_sched_find_best_target(p,
-							prefer_idle, min_util,
-							cpu, best_idle_cpu,
-							best_active_cpu, i);
+//					trace_sched_find_best_target(p,
+//							prefer_idle, min_util,
+//							cpu, best_idle_cpu,
+//							best_active_cpu, i);
 
 					return i;
 				}
@@ -7063,9 +7063,9 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		? best_active_cpu
 		: best_idle_cpu;
 
-	trace_sched_find_best_target(p, prefer_idle, min_util, cpu,
-				     best_idle_cpu, best_active_cpu,
-				     target_cpu);
+//	trace_sched_find_best_target(p, prefer_idle, min_util, cpu,
+//				     best_idle_cpu, best_active_cpu,
+//				     target_cpu);
 
 	schedstat_inc(p, se.statistics.nr_wakeups_fbt_count);
 	schedstat_inc(this_rq(), eas_stats.fbt_count);
