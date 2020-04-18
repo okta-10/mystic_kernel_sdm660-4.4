a3968aeb333e58e94a423d7e59122246cf3cc8a5
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-04-18 09:29:12 +0700

    Revert "mdss: boost devfreq only if the last input was within 3250ms"
    This reverts commit 1e7fd4e7c4c82e6cdaf4dabb25d7e2e12a283099.
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -5890,9 +5890,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		break;
 	case MSMFB_ATOMIC_COMMIT:
 		cpu_input_boost_kick();
-		if (time_before(jiffies, last_input_jiffies + msecs_to_jiffies(3250))) {
-			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-		}
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
