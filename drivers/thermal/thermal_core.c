6c11872793e91d7161aa777e41597d012c3dfe9d
psndna88 <psndna88@gmail.com>
2020-06-09 00:47:22 +0530

    thermal_core: kill userspace thermal-engine interference
    Signed-off-by: psndna88 <psndna88@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1023,7 +1023,7 @@ sconfig_store(struct device *dev, struct device_attribute *devattr,
 	if (kstrtoint(buf,10,&sconfig))
 		return -EINVAL;
 
-	thermal_msg->sconfig = sconfig;
+//	thermal_msg->sconfig = sconfig;
 
 	return count;
 }
@@ -1048,7 +1048,7 @@ temp_state_store(struct device *dev, struct device_attribute *devattr,
 	if (kstrtoint(buf,10,&temp_state))
 		return -EINVAL;
 
-	thermal_msg->temp_state = temp_state;
+//	thermal_msg->temp_state = temp_state;
 
 	return count;
 }
