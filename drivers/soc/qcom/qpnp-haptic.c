37dbd02709321feb945dbd492e6ef005639a2391
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-06-09 12:58:28 +0700

    drivers/haptic: add lavender support
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/soc/qcom/qpnp-haptic.c
+++ drivers/soc/qcom/qpnp-haptic.c
@@ -2312,6 +2312,14 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int time_ms)
 	qpnp_hap_vmax_config(hap, vmax_mv, false);
 #else
 
+#ifdef CONFIG_KERNEL_CUSTOM_F7A
+
+    pr_info("zjl hasfasda F7A haptic  =%d\n",time_ms);
+
+ 	vmax_mv = hap->vmax_mv;
+	qpnp_hap_vmax_config(hap, vmax_mv, false);
+#else
+
 #ifdef CONFIG_KERNEL_CUSTOM_E7S
 
     pr_info("zjl hasfasda E7S haptic  =%d\n",time_ms);
@@ -2386,6 +2394,7 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int time_ms)
     pr_info("zjl jjjjjj   haptic  =%d\n",time_ms);
 	if (is_sw_lra_auto_resonance_control(hap))
 		hrtimer_cancel(&hap->auto_res_err_poll_timer);
+#endif
 #endif
 #endif
 #endif
