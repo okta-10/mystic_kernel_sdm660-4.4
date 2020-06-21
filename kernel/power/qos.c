a1fc3c8fc7b0489506448504143fc5d67c2e0c6f
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-06-21 18:19:43 +0700

    Revert "disable tracing/logging at various places Part 2"
    This reverts commit a17b42212fb34a1a083ca63f14328901fddc8a8c.

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -358,7 +358,7 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 
 	spin_unlock(&pm_qos_lock);
 
-//	trace_pm_qos_update_target(action, prev_value, curr_value);
+	trace_pm_qos_update_target(action, prev_value, curr_value);
 	/*
 	 * if cpu mask bits are set, call the notifier call chain
 	 * to update the new qos restriction for the cores
@@ -434,7 +434,7 @@ bool pm_qos_update_flags(struct pm_qos_flags *pqf,
 
 	spin_unlock(&pm_qos_lock);
 
-//	trace_pm_qos_update_flags(action, prev_value, curr_value);
+	trace_pm_qos_update_flags(action, prev_value, curr_value);
 	return prev_value != curr_value;
 }
 
@@ -502,7 +502,7 @@ EXPORT_SYMBOL(pm_qos_request_for_cpumask);
 static void __pm_qos_update_request(struct pm_qos_request *req,
 			   s32 new_value)
 {
-//	trace_pm_qos_update_request(req->pm_qos_class, new_value);
+	trace_pm_qos_update_request(req->pm_qos_class, new_value);
 
 	if (new_value != req->node.prio)
 		pm_qos_update_target(
@@ -617,7 +617,7 @@ void pm_qos_add_request(struct pm_qos_request *req,
 
 	req->pm_qos_class = pm_qos_class;
 	INIT_DELAYED_WORK(&req->work, pm_qos_work_fn);
-//	trace_pm_qos_add_request(pm_qos_class, value);
+	trace_pm_qos_add_request(pm_qos_class, value);
 	pm_qos_update_target(pm_qos_array[pm_qos_class]->constraints,
 			     req, PM_QOS_ADD_REQ, value);
 
@@ -686,8 +686,8 @@ void pm_qos_update_request_timeout(struct pm_qos_request *req, s32 new_value,
 
 	cancel_delayed_work_sync(&req->work);
 
-//	trace_pm_qos_update_request_timeout(req->pm_qos_class,
-//					    new_value, timeout_us);
+	trace_pm_qos_update_request_timeout(req->pm_qos_class,
+					    new_value, timeout_us);
 	if (new_value != req->node.prio)
 		pm_qos_update_target(
 			pm_qos_array[req->pm_qos_class]->constraints,
@@ -727,7 +727,7 @@ void pm_qos_remove_request(struct pm_qos_request *req)
 	}
 #endif
 
-//	trace_pm_qos_remove_request(req->pm_qos_class, PM_QOS_DEFAULT_VALUE);
+	trace_pm_qos_remove_request(req->pm_qos_class, PM_QOS_DEFAULT_VALUE);
 	pm_qos_update_target(pm_qos_array[req->pm_qos_class]->constraints,
 			     req, PM_QOS_REMOVE_REQ,
 			     PM_QOS_DEFAULT_VALUE);
