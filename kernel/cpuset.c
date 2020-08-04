8ae14fa7c203d4b94e3017cdcef0aae34484bcfe
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-08-04 19:13:40 +0700

    Revert "CpuAssist & StuneAssist: Tweak values and add schedtune.sched_boost support"
    This reverts commit 968960b40b53f8249cb414999b4b3278008ef85f.

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1773,10 +1773,10 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 		{ "background",		"0-1" },
 		{ "audio-app",		"0-3" },
 		{ "camera-daemon",	"0-3" },
-		{ "system-background",	"0-2" },
-		{ "restricted",		"0-3" },
+		{ "system-background",	"0-3" },
+		{ "restricted",		"0-5" },
 		{ "top-app",		"0-7" },
-		{ "foreground",		"0-2,4-7" },
+		{ "foreground",		"0-3,6-7" },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
