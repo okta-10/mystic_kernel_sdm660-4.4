2c940d5053a68e08a5661becdac236a96d4b5cf2
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-08-14 16:23:44 +0700

    fs/proc: kill snapcam (gcam) and android.settings
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1261,7 +1261,9 @@ err_task_lock:
 out:
 	/* These apps burn through CPU in the background. Don't let them. */
 	if (!err && oom_score_adj >= 700) {
-		if (!strcmp(task_comm, "id.GoogleCamera")) {
+		if (!strcmp(task_comm, "id.GoogleCamera") ||
+		    !strcmp(task_comm, "eaurora.snapcam") ||
+		    !strcmp(task_comm, "ndroid.settings")) {
 			struct task_kill_info *kinfo;
 
 			kinfo = kmalloc(sizeof(*kinfo), GFP_KERNEL);
