20d44a1b84e44a2eba93033d9d2766b781ffd97d
Ashay Jaiswal <ashayj@codeaurora.org>
2020-09-04 20:02:32 +0700

    qcom: smb138x/smb1351: add support to expose BATFET configuration
    Add support to expose how BATFET of main and parallel chargers
    are configured. BATFET configuration is used for configuring
    FCC of main charger.
    
    Change-Id: I1fa47840dee08754425dc4540a61401313f0878a
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/power/supply/qcom/smb1351-charger.c
+++ drivers/power/supply/qcom/smb1351-charger.c
@@ -462,6 +462,7 @@ struct smb1351_charger {
 
 	int			parallel_pin_polarity_setting;
 	int			parallel_mode;
+	int			pl_batfet_mode;
 	bool			parallel_charger;
 	bool			parallel_charger_suspended;
 	bool			bms_controlled_charging;
@@ -1418,6 +1419,7 @@ static enum power_supply_property smb1351_parallel_properties[] = {
 	POWER_SUPPLY_PROP_CHARGE_TYPE,
 	POWER_SUPPLY_PROP_PARALLEL_MODE,
 	POWER_SUPPLY_PROP_INPUT_SUSPEND,
+	POWER_SUPPLY_PROP_PARALLEL_BATFET_MODE,
 	POWER_SUPPLY_PROP_MODEL_NAME,
 };
 
@@ -1731,6 +1733,9 @@ static int smb1351_parallel_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 		val->intval = chip->parallel_charger_suspended;
+		break;
+	case POWER_SUPPLY_PROP_PARALLEL_BATFET_MODE:
+		val->intval = chip->pl_batfet_mode;
 		break;
 	case POWER_SUPPLY_PROP_MODEL_NAME:
 		val->strval = "smb1351";
@@ -3235,6 +3240,10 @@ static int smb1351_parallel_charger_probe(struct i2c_client *client,
 		chip->parallel_mode = POWER_SUPPLY_PL_USBIN_USBIN_EXT;
 	else
 		chip->parallel_mode = POWER_SUPPLY_PL_USBIN_USBIN;
+
+	chip->pl_batfet_mode = POWER_SUPPLY_PL_NON_STACKED_BATFET;
+	if (of_property_read_bool(node, "qcom,stacked-batfet"))
+		chip->pl_batfet_mode = POWER_SUPPLY_PL_STACKED_BATFET;
 
 	i2c_set_clientdata(client, chip);
 
