b467f8fcf79f639c2a8cb72bdaebcb493608e525
LibXZR <xzr467706992@163.com>
2020-09-07 22:33:11 +0800

    devfreq_boost: Expose disable_boosts node
    * Allow userspace disable it if they want
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -9,6 +9,10 @@
 #include <linux/fb.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
+#include <linux/module.h>
+
+static bool disable_boosts __read_mostly;
+module_param(disable_boosts, bool, 0644);
 
 enum {
 	SCREEN_OFF,
@@ -65,6 +69,9 @@ static void __devfreq_boost_kick(struct boost_dev *b)
 void devfreq_boost_kick(enum df_device device)
 {
 	struct df_boost_drv *d = &df_boost_drv_g;
+	
+	if (disable_boosts)
+		return;
 
 	__devfreq_boost_kick(d->devices + device);
 }
@@ -97,6 +104,9 @@ static void __devfreq_boost_kick_max(struct boost_dev *b,
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
 {
 	struct df_boost_drv *d = &df_boost_drv_g;
+	
+	if (disable_boosts)
+		return;
 
 	__devfreq_boost_kick_max(d->devices + device, duration_ms);
 }
