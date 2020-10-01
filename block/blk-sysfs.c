48f27363230267276ede938bcedc504f45269bfe
Adam W. Willis <return.of.octobot@gmail.com>
2020-10-01 19:24:47 +0800

    mm: Reduce VM_MAX_READAHEAD to 128KB and apply init protection
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -92,6 +92,9 @@ queue_ra_store(struct request_queue *q, const char *page, size_t count)
 
 	q->backing_dev_info->ra_pages = ra_kb >> (PAGE_SHIFT - 10);
 
+	if (!strcmp(current->comm, "init"))
+		ra_kb = VM_MAX_READAHEAD;
+
 	return ret;
 }
 
