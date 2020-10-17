8c9094514c4713049c6dfb628d5d756957d0fc2d
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-10-17 09:02:10 +0700

    drivers/msm_thermal: do not make CPU offline
    whatever cpu task, e.g when running cpu throttling test, cpu will go offline when temp reaches 40°C above, and it also happens when playing games, remember that does not mean it also increases in performance.
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -2338,6 +2338,7 @@ static ssize_t ocr_reg_mode_store(struct kobject *kobj,
 		pr_err("Invalid value %d for mode\n", val);
 		goto done_ocr_store;
 	}
+		goto done_ocr_store;
 
 	if (val != reg->mode) {
 		ret = request_optimum_current(reg, val);
@@ -2432,6 +2433,7 @@ static ssize_t psm_reg_mode_store(struct kobject *kobj,
 		pr_err("Invalid number %d for mode\n", val);
 		goto done_psm_store;
 	}
+		goto done_psm_store;
 
 	if (val != reg->mode) {
 		ret = rpm_regulator_set_mode(reg->reg, val);
@@ -5006,6 +5008,7 @@ static ssize_t __ref store_cc_enabled(struct kobject *kobj,
 		pr_info("Core control enabled\n");
 		cpus_previously_online_update();
 		register_cpu_notifier(&msm_thermal_cpu_notifier);
+		goto done_store_cc;
 		/*
 		 * Re-evaluate thermal core condition, update current status
 		 * and set threshold for all cpus.
@@ -5060,6 +5063,7 @@ static ssize_t __ref store_cpus_offlined(struct kobject *kobj,
 		pr_err("Ignoring request; polling thread is enabled.\n");
 		goto done_cc;
 	}
+		goto done_cc;
 
 	for_each_possible_cpu(cpu) {
 		if (!(msm_thermal_info.core_control_mask & BIT(cpu)))
