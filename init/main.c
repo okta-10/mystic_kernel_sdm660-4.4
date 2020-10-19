e37e321287d8dad2870169a5cd23d2df9264fc30
psndna88 <psndna88@gmail.com>
2020-10-19 17:07:12 +0530

    ASoC: msm: add alternative wired headphone buttons mode
    Needed for roms like Nusantara
    
    Signed-off-by: psndna88 <psndna88@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- init/main.c
+++ init/main.c
@@ -130,6 +130,25 @@ static char *initcall_command_line;
 static char *execute_command;
 static char *ramdisk_execute_command;
 
+bool wired_btn_altmode = false;
+static int __init set_wiredbtnmode(char *val)
+{
+	unsigned int temp;
+
+	get_option(&val, &temp);
+
+	if (temp) {
+		wired_btn_altmode = true;
+		pr_info("Kernel: alternate wired button mode = 1");
+	} else {
+		wired_btn_altmode = false;
+		pr_info("Kernel: alternate wired button mode = 0");
+	}
+
+	return 0;
+}
+__setup("androidboot.wiredbtnaltmode=", set_wiredbtnmode);
+
 /*
  * Used to generate warnings if static_key manipulation functions are used
  * before jump_label_init is called.
