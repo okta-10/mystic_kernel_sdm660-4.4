465fd6f73b6a949796149ed428fd9ff32f091553
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-04-13 08:31:09 +0700

    Revert "cpu_input_boost: Disable energy aware when kick max boost"
    This reverts commit b37bc9c8425be66a84b1a74b04d2bb24796ca430.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -11,7 +11,6 @@
 #include <linux/input.h>
 #include <linux/kthread.h>
 #include <linux/moduleparam.h>
-#include <linux/sched/sysctl.h>
 #include <linux/sched.h>
 
 static unsigned int input_boost_freq_lp __read_mostly =
@@ -153,8 +152,6 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 {
 	struct boost_drv *b = &boost_drv_g;
 
-	sysctl_sched_energy_aware = 0;
-
 	__cpu_input_boost_kick_max(b, duration_ms);
 }
 
@@ -167,8 +164,6 @@ static void input_unboost_worker(struct work_struct *work)
 	if (dynamic_sched_boost)
 		sched_set_boost(0);
 	wake_up(&b->boost_waitq);
-
-	sysctl_sched_energy_aware = 1;
 }
 
 static void max_unboost_worker(struct work_struct *work)
