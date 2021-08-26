347d149a157e150ff2bbc5bb42fbf02645826b31
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-08-26 18:52:11 +0700

    Revert "mm: Reduce VM_MAX_READAHEAD to 128KB and apply init protection"
    This reverts commit 72b67e9467f94619ec849ac62f506469e62ae93d.
    
    Revert "mm: Reduce VM_MAX_READAHEAD to 128KB and apply init protection"
    
    This reverts commit 48f27363230267276ede938bcedc504f45269bfe.

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -90,13 +90,7 @@ queue_ra_store(struct request_queue *q, const char *page, size_t count)
 	if (ret < 0)
 		return ret;
 
-	if (!strcmp(current->comm, "init"))
-		ra_kb = VM_MAX_READAHEAD;
-
 	q->backing_dev_info->ra_pages = ra_kb >> (PAGE_SHIFT - 10);
-
-	if (!strcmp(current->comm, "init"))
-		ra_kb = VM_MAX_READAHEAD;
 
 	return ret;
 }
